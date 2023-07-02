466b78f25d8815a4c4e45bf54707fb668c110eb0
gduin <giovannyduin@gmail.com>
2023-07-02 00:35:22 -0400

    Final change

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -9,10 +9,12 @@ class translator_test(unittest.TestCase):
     def test_e2f(self):
     
         self.assertEqual( translator.translate_english_to_french(self.english_text), self.french_text)
+        self.assertNotEqual( translator.translate_english_to_french(self.english_text), self.english_text)
 
     def test_f2e(self):
         
         self.assertEqual( translator.translate_french_to_english(self.french_text), self.english_text)
+        self.assertNotEqual( translator.translate_french_to_english(self.french_text), self.french_text)
 
 if __name__ == '__main__':
     unittest.main()
